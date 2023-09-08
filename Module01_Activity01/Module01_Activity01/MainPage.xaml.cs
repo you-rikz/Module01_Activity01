8aa83f8fdb9d70a7470f6fe0ff7425d40601977a
you-rikz <arnoldlim0337@gmail.com>
2023-09-08 23:51:25 +0800

    first commit

---
+++ Module01_Activity01/Module01_Activity01/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Module01_Activity01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OpenPage1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+        private async void OpenPage2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
